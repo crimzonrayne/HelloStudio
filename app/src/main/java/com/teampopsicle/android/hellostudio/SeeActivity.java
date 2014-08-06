29805e295d5c3126600f5a44174652c1e56b8452
Andrew H <Heider.Andrew@gmail.com>
2014-08-05 18:28:34 -0700

    Adding two new Activities

---
+++ app/src/main/java/com/teampopsicle/android/hellostudio/SeeActivity.java
@@ -0,0 +1,19 @@
+package com.teampopsicle.android.hellostudio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * Created by Crimzon on 8/5/2014.
+ */
+
+public class SeeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see);
+    }
+
+}
